9c138e5fbc3dc7d1e2cba248454e0f32fb7ab362
afe1 <yunfei.zhu@nwowtec.com>
2024-08-26 09:18:19 +0800

    js slice

---
+++ jsDemo/slice.js
@@ -0,0 +1,15 @@
+const a = [0, 1, 2, 3, 4, 5]
+
+const b = a.slice(1, 3)
+
+const c = a.slice(2)
+
+const d = a.slice(-2)
+
+const e = a.slice()
+
+console.log('a :>> ', a)
+console.log('b :>> ', b)
+console.log('c :>> ', c)
+console.log('d :>> ', d)
+console.log('e :>> ', e)

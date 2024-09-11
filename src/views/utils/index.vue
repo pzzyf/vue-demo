75afd832bd28a0ad0a023a8ce99c55c7db4c37e6
pzzyf <2279948211@qq.com>
2024-09-11 17:02:23 +0800

    feat: afe1 utils demo

---
+++ src/views/utils/index.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { arr_unique } from '@afe1/utils'
+
+const is = arr_unique([1, 2, 3, 2, 666])
+</script>
+
+<template>
+  <div>
+    <h1>Utils</h1>
+    <p>{{ is }}</p>
+  </div>
+</template>
+
+<style scoped></style>

bf41b109498ca6615244e83c20bf0c3522f487ad
afe1 <yunfei.zhu@nwowtec.com>
2024-09-28 11:00:02 +0800

    feat: promise类方法

---
+++ jsDemo/promise.js
@@ -0,0 +1,31 @@
+const arr = [
+  1,
+  2,
+  3,
+  4,
+  5,
+]
+
+async function test() {
+  return Promise.resolve(arr)
+}
+
+async function test1() {
+  return Promise.reject(arr)
+}
+
+(async () => {
+  const a = await test()
+  const b = test()
+
+  console.log('test() :>> ', test())
+  console.log('a :>> ', a)
+  console.log('b :>> ', b)
+
+  const c = await test1()
+  const d = test1()
+
+  console.log('test1() :>> ', test1())
+  console.log('c :>> ', c)
+  console.log('d :>> ', d)
+})()
